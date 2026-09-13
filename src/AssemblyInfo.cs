4c9b517a53f1ab698a41f5e8ec7aa81b5b7738b2
SimpleShot <simple-shot@local>
2026-09-13 21:59:19 +0800

    release: v1.2.3

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Reflection;
 [assembly: AssemblyProduct("SnapCut")]
 [assembly: AssemblyCompany("SnapCut")]
 [assembly: AssemblyDescription("快截 - WeChat-style screenshot, scrolling capture, OCR and screen recorder")]
-[assembly: AssemblyVersion("1.2.2")]
-[assembly: AssemblyFileVersion("1.2.2")]
+[assembly: AssemblyVersion("1.2.3")]
+[assembly: AssemblyFileVersion("1.2.3")]
